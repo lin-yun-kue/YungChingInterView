94503a84fbf7606dfd2b5d4e6623f445feebfa91
Kyle.Lin <b40885@gmail.com>
2024-03-13 16:52:42 +0800

    add ef and sqlite

---
+++ YungChing/Controllers/CompanyController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YungChing.EFModel;
+
+namespace YungChing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly CompanyContext _companyContext;
+
+        public CompanyController(CompanyContext context) 
+        {
+            _companyContext = context;
+        }
+
+        [HttpGet(Name = "GetAll")]
+        public IEnumerable<Company> Get() {
+            return _companyContext.Company.ToList();
+        }
+    }
+}
